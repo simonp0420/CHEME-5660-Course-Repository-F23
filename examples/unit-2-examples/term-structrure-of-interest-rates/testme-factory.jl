2cbd6d023f7d7639db71ac8c0461bffa2b44aeb9
Jeffrey Varner <jdv27@cornell.edu>
2023-05-23 15:52:11 -0400

    Update testme-factory.jl
    even

--- examples/unit-2-examples/term-structrure-of-interest-rates/testme-factory.jl
+++ examples/unit-2-examples/term-structrure-of-interest-rates/testme-factory.jl
@@ -4,7 +4,7 @@ include("Include.jl");
 # initialize -
 u = 1.25;    # up factor
 d = 0.90;    # down factor
-p = 0.5;     # probability of an up move
+p = 0.50;     # probability of an up move
 rₒ = 0.06;   # root value
 T = 4;       # number of time periods
 Vₚ = 100.0;  # par value of the T-bill
